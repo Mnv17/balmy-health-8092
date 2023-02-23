75ca9a0dc61df11d8fc178bd3c34d8618b185e31
Mnv17 <maanavverma0@gmail.com>
2023-02-24 01:59:29 +0530

    Day-3 work completed

--- furniture_home/src/App.js
+++ furniture_home/src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
-import {Navbar} from './Components/navbar';
+import { Navbar } from './Components/navbar';
+import { MiddlePart } from "./Components/middle";
+import { Footer } from './Components/footer';
+import { MiddleDiv } from './Components/middle1';
 
 function App() {
   return (
-    <>
+  <>
     <Navbar/>
+    <MiddlePart/>
+    <MiddleDiv/>
+    <Footer/>
     </>
      
     
---
+++ furniture_home/src/Components/middle.jsx
@@ -0,0 +1,12 @@
+import "./middle.css"
+
+export function MiddlePart(){
+    return(
+    <div >
+    <div> 
+        <h1>MiddlePart</h1>  
+    </div>
+    
+    </div>
+    )
+}
